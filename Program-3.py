c1f1cc8172c184d1c4dc8fd50233f8995fd2d208
AviGautam1 <avifetian@gmail.com>
2022-12-27 10:19:10 -0800

    Write a Python program to find the first repeated character in a given string.

---
+++ Program-3.py
@@ -0,0 +1,24 @@
+# Write a Python program to find the first repeated character in a given string.
+
+
+my_string = input("Enter A String : ")
+
+for index, letter1 in enumerate(my_string):
+    for letter2 in my_string[index+1:]:
+        if letter1 == letter2:
+            print("First Repeated Character Is : ", letter1)
+            exit(0)
+
+else:
+    print("String Does Not Have Repoeated Characters")
+
+'''
+output = 
+Case-1
+Enter A String : hello
+First Repeated Character Is :  l
+
+Case-2
+Enter A String : avinesh
+String Does Not Have Repoeated Characters
+'''
