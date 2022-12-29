bf723d290ce450b174248ec0e21e766ba3953604
AviGautam1 <avifetian@gmail.com>
2022-12-29 00:39:23 -0800

    Write a Python program to remove unwanted characters from a given string.

--- Program-32.py
+++ Program-32.py
@@ -1,3 +1,22 @@
+# Write a Python program to remove unwanted characters from a given string.
+
+string = input("Enter A Unwanted String : ")
+print("After Removing Unwanted Character : ", string)
+
+'''
+Python String isalnum() method checks whether all the characters in a given string are alphanumeric or not. 
+It returns a boolean as True – If all the characters are alphanumeric or else false – 
+If one or more characters are not alphanumeric.
+'''
+
+test_str = ''.join(letter for letter in string if letter.isalnum())
+
+print("After Removing Unwanted Character : ", test_str)
 
 
-
+'''
+output = 
+Enter A Unwanted String : Pyth*^on Exercis^es
+After Removing Unwanted Character :  Pyth*^on Exercis^es
+After Removing Unwanted Character :  PythonExercises
+'''
