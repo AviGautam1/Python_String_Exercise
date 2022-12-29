d29af81f33d02a76b2d89ba8f9e40d157305c406
AviGautam1 <avifetian@gmail.com>
2022-12-29 02:31:54 -0800

    Write a Python program to split a given multiline string into a list of lines.

---
+++ Program-41.py
@@ -0,0 +1,17 @@
+# Write a Python program to split a given multiline string into a list of lines.
+
+
+# Use str.split() and '\n' to match line breaks and create a list.
+str = "Avinesh\nGautam\nIs A\nCoder"
+print(str)
+print(str.split('\n'))
+
+
+'''
+output = 
+Avinesh
+Gautam
+Is A
+Coder
+['Avinesh', 'Gautam', 'Is A', 'Coder']
+'''
