6a895a895ff260a088ea221d739a82dc1a6da491
AviGautam1 <avifetian@gmail.com>
2022-12-29 03:00:35 -0800

    Write a Python program that capitalizes the first letter and lowercases the remaining letters of a given string.

---
+++ Program-45.py
@@ -0,0 +1,11 @@
+# Write a Python program that capitalizes the first letter and lowercases the remaining letters of a given string.
+
+my_string = input("Enter A String : ")
+
+print(my_string[0].upper() + my_string[1:].lower())
+
+'''
+output = 
+Enter A String : AvineSH gAUtam
+Avinesh gautam
+'''
